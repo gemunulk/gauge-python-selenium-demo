d414de8231bf2414960ef22a2e29c7e7c59ad6ad
gemunulk <gemunu.lk@gmail.com>
2019-03-03 16:06:53 +0530

    Minimal setup.

--- step_impl/pages/page_factory.py
+++ step_impl/pages/page_factory.py
@@ -20,12 +20,6 @@ class PageFactory:
 def init():
     chrome_options = Options()  
     chrome_options.add_argument("--headless")  
-    chrome_options.add_argument("--window-size=1920x1080")
-    #All the arguments added for chromium to work on selenium
-    #chrome_options.add_argument("--no-sandbox") #This make Chromium reachable
-    #chrome_options.add_argument("--no-default-browser-check") #Overrides default choices
-    #chrome_options.add_argument("--no-first-run")
-    #chrome_options.add_argument("--disable-default-apps") 
 
     if platform.system() == 'Darwin':
         chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' 
